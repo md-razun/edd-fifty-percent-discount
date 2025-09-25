320be07214e46ea5eb7a1df1a529f4cf63d90297
Md. Razun Mia <147147677+md-razun@users.noreply.github.com>
2025-09-25 19:20:20 +0600

    change text

--- edd-fifty-percent-discount.php
+++ edd-fifty-percent-discount.php
@@ -97,7 +97,7 @@ function fifty_percent_discount_apply_special_discount() {
         $discount_amount = $cart_total * 0.5;
         EDD()->fees->add_fee( array(
                 'amount' => -$discount_amount,
-                'label'  => 'Special Discount for South Asia',
+                'label'  => '50% Off for South Asia',
                 'id'     => 'special_discount',
                 'type'   => 'discount',
                 'no_tax' => true
